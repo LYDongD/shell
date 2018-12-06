8fbbbfc2054df2040044200ccf388a8ca077533e
金雯 <554050334@qq.com>
2018-12-07 07:40:13 +0800

    add new shell

---
+++ curl/img_downloader.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# 图片crawler
+
+#param check
+if [ $# -ne 3 ];
+then
+    echo "Usage: $0 URL -d DIRECTORY"
+    exit -1
+fi
+
+
+#param parse
+for i in {1..4}
+do
+    case $1 in 
+    -d)shift;
+    directory=$1;shift;;
+    *)url=$1{url:-$1};shift;
+    esac
+done
+
+mkdir -p $directory
+baseurl=$(echo $url | egrep -o "https?://[a-z.]+")
+
+#request
+curl -s $url | egrep -o "<img src=[^>]*>" | sed 's/<img src=\"\([^"]\).*/\1/g' > /tmp/$$.list
+
+
